7d1c92a08b9e63cf6bdcc809bf69d33d16b69a95
Majd Taby <majd@darkroom.co>
2019-08-02 18:52:42 +0200

    WIP

--- FirstYear/ItemDisplayManager.m
+++ FirstYear/ItemDisplayManager.m
@@ -8,7 +8,8 @@
 
 #import "ItemDisplayManager.h"
 
-#define SONGINTRO 1.5
+#define WORDINTRO 1.5 + 1.0
+#define SONGINTRO (WORDINTRO + 4.10 + 1.0)
 
 @interface ItemDisplayManager ()
 
@@ -31,14 +32,25 @@
                       [[Item alloc] initWithTimeToShow:0.0 duration:0.5 messageType:MessageTypeString message:@"Hi"],
                       [[Item alloc] initWithTimeToShow:0.5 duration:1. messageType:MessageTypeString message:@"Hi Amadea."],
                       
-                      // t=0.4
+                      [[Item alloc] initWithTimeToShow:WORDINTRO + 0.05 duration:.5 messageType:MessageTypeString  message:@"I've\n\n"],
+                      [[Item alloc] initWithTimeToShow:WORDINTRO + 0.10 duration:.8 messageType:MessageTypeString  message:@"I've cried\n\n"],
+                      [[Item alloc] initWithTimeToShow:WORDINTRO + 0.85 duration:.5 messageType:MessageTypeString  message:@"I've cried\na\n"],
+                      [[Item alloc] initWithTimeToShow:WORDINTRO + 0.88 duration:.5 messageType:MessageTypeString  message:@"I've cried\na few\n"],
+                      [[Item alloc] initWithTimeToShow:WORDINTRO + 0.93 duration:.5 messageType:MessageTypeString  message:@"I've cried\na few times\n"],
+                      [[Item alloc] initWithTimeToShow:WORDINTRO + 1.07 duration:.5 messageType:MessageTypeString  message:@"I've cried\na few times\nin"],
+                      [[Item alloc] initWithTimeToShow:WORDINTRO + 1.13 duration:.5 messageType:MessageTypeString  message:@"I've cried\na few times\nin our"],
+                      [[Item alloc] initWithTimeToShow:WORDINTRO + 1.20 duration:0.8 messageType:MessageTypeString  message:@"I've cried\na few times\nin our relationship."],
+                      
+                      [[Item alloc] initWithTimeToShow:WORDINTRO + 2.0 duration:0.8 messageType:MessageTypeString  message:@"But"],
+                      [[Item alloc] initWithTimeToShow:WORDINTRO + 2.10 duration:2.0 messageType:MessageTypeString  message:@"But today..."],
+                      
                       [[Item alloc] initWithTimeToShow:SONGINTRO + 0.00 duration:120. messageType:MessageTypeSong  message:@"CuzILoveYou"],
-                      [[Item alloc] initWithTimeToShow:SONGINTRO + 0.50 duration:8.0 messageType:MessageTypeString message:@"I'm\n"],
-                      [[Item alloc] initWithTimeToShow:SONGINTRO + 1.16 duration:8.0 messageType:MessageTypeString message:@"I'm cryin\n"],
-                      [[Item alloc] initWithTimeToShow:SONGINTRO + 2.33 duration:8.0 messageType:MessageTypeString message:@"I'm cryin\n'cause"],
-                      [[Item alloc] initWithTimeToShow:SONGINTRO + 3.00 duration:8.0 messageType:MessageTypeString message:@"I'm cryin\n'cause I"],
-                      [[Item alloc] initWithTimeToShow:SONGINTRO + 3.30 duration:8.0 messageType:MessageTypeString message:@"I'm cryin\n'cause I love"],
-                      [[Item alloc] initWithTimeToShow:SONGINTRO + 4.60 duration:3.9 messageType:MessageTypeString message:@"I'm cryin\n'cause I love you."],
+                      [[Item alloc] initWithTimeToShow:SONGINTRO + 0.50 duration:2.0 messageType:MessageTypeString message:@"I'm\n"],
+                      [[Item alloc] initWithTimeToShow:SONGINTRO + 1.16 duration:2.0 messageType:MessageTypeString message:@"I'm cryin\n"],
+                      [[Item alloc] initWithTimeToShow:SONGINTRO + 2.33 duration:2.0 messageType:MessageTypeString message:@"I'm cryin\n'cause"],
+                      [[Item alloc] initWithTimeToShow:SONGINTRO + 3.00 duration:2.0 messageType:MessageTypeString message:@"I'm cryin\n'cause I"],
+                      [[Item alloc] initWithTimeToShow:SONGINTRO + 3.30 duration:2.0 messageType:MessageTypeString message:@"I'm cryin\n'cause I love"],
+                      [[Item alloc] initWithTimeToShow:SONGINTRO + 4.60 duration:3.0 messageType:MessageTypeString message:@"I'm cryin\n'cause I love you."],
                       
                       [[Item alloc] initWithTimeToShow:SONGINTRO + 6.23 duration:8.0 messageType:MessageTypeCircle message:nil],
                       [[Item alloc] initWithTimeToShow:SONGINTRO + 7.33 duration:8.0 messageType:MessageTypeCircle message:nil],
